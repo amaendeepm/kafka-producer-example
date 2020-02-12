1d3d2d53fdf6c58b1b10110b7d9dd39efe1a0b8d
martincallesen <mail@martincallesen.dk>
2020-02-12 15:14:47 +0100

    added creating account topic in spring

--- src/main/java/dk/martincallesen/kafka/producer/KafkaProducerConfig.java
+++ src/main/java/dk/martincallesen/kafka/producer/KafkaProducerConfig.java
@@ -2,12 +2,14 @@ package dk.martincallesen.kafka.producer;
 
 import dk.martincallesen.kafka.domain.Account;
 import dk.martincallesen.kafka.serializer.AvroSerializer;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -40,5 +42,14 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, Account> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic accountTopic() {
+        return TopicBuilder.name("account")
+                .partitions(10)
+                .replicas(1)
+                .compact()
+                .build();
     }
 }
--- src/test/java/dk/martincallesen/kafka/KafkaProducerApplicationIT.java
+++ src/test/java/dk/martincallesen/kafka/KafkaProducerApplicationIT.java
@@ -1,12 +1,19 @@
 package dk.martincallesen.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KafkaProducerApplicationIT {
 
+    @Autowired
+    private NewTopic accountTopic;
+
     @Test
-    void contextLoads() {
+    void isAccountTopicCreated(){
+        Assertions.assertNotNull(accountTopic);
     }
 }
